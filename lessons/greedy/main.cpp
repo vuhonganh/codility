ba9829afa22505c3d1dba5b33fc7903e261b710b
Hong Anh VU <vuhonganh91@gmail.com>
2016-01-02 23:21:21 +0100

    modify: lessons/greedy: max non-overlapping segments problems: add greedy versions, score 100%

--- lessons/greedy/main.cpp
+++ lessons/greedy/main.cpp
@@ -41,3 +41,40 @@ int sol_maxNonOverlappingSegs(vector<int> &A, vector<int> &B)
     }
   return maxVal;
 }
+
+/* GREEDY PROOF: ( from codesays.com )
+  Let’s scan from left to right, according to the end points. Since the input has already been sorted with the end points, linear scan works. Firstly, we define the cluster as: let the first item of that segment be segment[i], the cluster is all the segments from segment[i] to segment[j] (both inclusive, and i <= j), such that:
+for all x (i < x <= j), segment[i].begin <= segment[x].begin <= segment[i].end
+
+Any two segments in one cluster are overlapping.
+1. Because the segments are sorted as the end point, (that is, for all x (i < x <= j), segment[x].end >= segment[i].end), and the definition of cluster, all the other segments are overlapping with the first one.
+2. Any two no-first segments are overlapping. Because both their begin points are <= segment[i].end, and both end points are >= segment[i].end.
+End of proof. As a result, in one cluster, we can pick up AT MOST one segment.
+
+In any two consecutive clusters, we can choose two segments MAXIMUMLY. Let cluster[i] be the i(th) cluster, and cluster[i].segment[j] be the j(th) segment in the i(th) cluster.
+1. We CAN choose two. As the definition of cluster, we have:
+cluster[i].segment[first].end < cluster[i+1].segment[first].begin
+And with the definition of segment, we have:
+cluster[i].segment[first].begin <= cluster[i].segment[first].end < cluster[i+1].segment[first].begin <= cluster[i+1].segment[first].end
+Therefore, they (cluster[i].segment[first] and cluster[i+1].segment[first]) are not overlapping.
+2. We can choose two AT MOST. If we can choose three or more non-overlapping segments in these two consecutive clusters, according to pigeonhole principle, at least two non-overlapping segments are in one cluster. But according to our previous discussion, it is impossible.
+
+Similarly, we can proof that: with N clusters, we can pick up N non-overlapping segments at most. Therefore, the orginal question becomes: find the number of clusters in the segments.
+*/
+int sol_maxNonOverlappingSegs_GREEDY(vector<int> &A, vector<int> &B) //100% 
+{
+  if (A.size() < 2) return A.size();
+  int cnt = 1;
+  int curPoint = B[0];
+  size_t idx = 1;
+  while (idx < A.size())
+    {
+      while (idx < A.size() && A[idx] <= curPoint) idx++;
+      if (idx < A.size())
+	{
+	  cnt++;
+	  curPoint = B[idx];
+	}
+    }
+  return cnt;
+}
